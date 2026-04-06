79bee5100ca0334fd70b8f4f2cb51a4e9c931b7a
anil11926 <anilkumar955321336@gmail.com>
2026-04-06 15:17:00 +0530

    feat: new class primitives added operators added

--- src/com/java/fundamentals/Operators.java
+++ src/com/java/fundamentals/Operators.java
@@ -29,6 +29,16 @@ public class Operators {
         System.out.println(num1);
         System.out.println(num2);
 
+        System.out.println("*****************************************************************************");
+
+
+        num1 = 32;
+        num2 = 65;
+        System.out.println( num1++ > num2 && num1 < num2 );
+
+
+
+
 
     }
 }
