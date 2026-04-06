3f3648c7a84e2d57aa42fb78b9a5a6bb722e5bfa
anil11926 <anilkumar955321336@gmail.com>
2026-04-06 15:01:47 +0530

    feat: new class primitives added operators added

---
+++ src/com/java/fundamentals/Operators.java
@@ -0,0 +1,34 @@
+package com.java.fundamentals;
+
+public class Operators {
+    static void main() {
+        byte no1 = 100;
+        byte no2;
+        no2 = no1;
+        System.out.println(no2);
+        int res = 10 + 20 - 30 * 40 / 50 % 60;
+        System.out.println(res);
+
+
+        System.out.println("=====================================================================================================");
+
+        int num1 = 342342342;
+        int num2 = 788624165;
+        long sum = num1 + num2;
+        System.out.println(sum);
+
+        byte b1 = 127;
+        byte b2 = 127;
+        short res1 = (short) (b1 + b2);
+        System.out.println(res);
+
+        num1 = 10;
+        num2 = 20;
+        res = --num1 + ++num1 + num1-- + num1++ - --num2 + ++num2;
+        System.out.println(res);
+        System.out.println(num1);
+        System.out.println(num2);
+
+
+    }
+}
