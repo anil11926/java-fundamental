f1e8b95d1ebbd8707def6a6174346311e54fed01
anil11926 <anilkumar955321336@gmail.com>
2026-04-15 13:28:46 +0530

    feat: added Print Even Numbers

--- src/com/java/fundamentals/PrintEvenNumbers.java
+++ src/com/java/fundamentals/PrintEvenNumbers.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PrintEvenNumbers {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the Range: ");
+        System.out.println("Enter the Range: ");
         int range = sc.nextInt();
         int number = 1;
         while (number <= range) {
