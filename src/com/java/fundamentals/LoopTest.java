bedf4b10964b9bd5885eec79c37b4f6c7931565f
anil11926 <anilkumar955321336@gmail.com>
2026-04-15 13:20:55 +0530

    feat: added Loop Test

--- src/com/java/fundamentals/LoopTest.java
+++ src/com/java/fundamentals/LoopTest.java
@@ -14,7 +14,7 @@ public class LoopTest {
  class LoopTest1 {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the range");
+        System.out.println("Enter the Range");
         int range = sc.nextInt();
         int number = 1;
         while(number <= range) {
