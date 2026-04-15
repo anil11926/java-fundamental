52cf6226ad1d77accca6d9083f68bb6e4942ac28
anil11926 <anilkumar955321336@gmail.com>
2026-04-15 12:25:25 +0530

    feat: new class primitives added operators added inputs testing,E-commerce app menu price calculator , loan calculation, loop test, Swith test, even numbers, sum of two numbers, count of digits, print digits, product data

---
+++ src/com/java/fundamentals/SumOftwonumbers.java
@@ -0,0 +1,18 @@
+package com.java.fundamentals;
+
+import java.util.Scanner;
+
+public class SumOftwonumbers {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("entert First number");
+        long firstNumber = sc.nextLong();
+        System.out.println("enter Second number");
+        long secondNumber = sc.nextLong();
+        long result = firstNumber + secondNumber;
+        System.out.println("Result: " + result);
+        sc.close();
+
+
+    }
+}
